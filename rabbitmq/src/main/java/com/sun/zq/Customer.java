5f0562b22a5c7c772795281d69809855fee77c9d
sz48726 <sz48726@ly.com>
2017-11-07 14:48:04 +0600

    提交项目‘

---
+++ rabbitmq/src/main/java/com/sun/zq/Customer.java
@@ -0,0 +1,45 @@
+package com.sun.zq;
+
+import com.rabbitmq.client.AMQP.BasicProperties;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Customer {
+    private static final String QUEUE_NAME="test.queue";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        // 创建连接工厂
+        ConnectionFactory factory = new ConnectionFactory();
+        //设置RabbitMQ地址
+        factory.setHost("localhost");
+        // 创建一个连接
+        Connection connection = factory.newConnection();
+        // 创建一个通道
+        Channel channel = connection.createChannel();
+        //声明要关注的队列
+
+        channel.queueDeclare(QUEUE_NAME,false,false,false,null);
+        System.out.println("Customer2 Waiting Received messages");
+
+        // 告诉服务器我们需要那个频道的消息，如果频道中有消息，就会执行回调函数handleDelivery
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties, byte[] body)
+                    throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println("Customer2 Received '" + message + "'");
+            }
+        };
+
+        //自动回复队列应答 -- RabbitMQ中的消息确认机制
+        channel.basicConsume(QUEUE_NAME,true,consumer);
+        channel.close();
+        connection.close();
+    }
+}
